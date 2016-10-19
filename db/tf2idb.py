95f81f36b7e90936b6c668a9b0be1b2c7825d283
FlaminSarge <flaminsarge@gmail.com>
2016-10-18 20:51:17 -0700

    Minor formatting changes

--- db/tf2idb.py
+++ db/tf2idb.py
@@ -43,7 +43,7 @@ def main():
     dbc.execute('CREATE TABLE "new_tf2idb_class" ("id" INTEGER NOT NULL , "class" TEXT NOT NULL , "slot" TEXT , PRIMARY KEY ("id", "class"))')
     dbc.execute('CREATE TABLE "new_tf2idb_item_attributes" ("id" INTEGER NOT NULL , "attribute" INTEGER NOT NULL , "value" TEXT NOT NULL, PRIMARY KEY ("id", "attribute") )')
     dbc.execute('CREATE TABLE "new_tf2idb_item" ('
-        '"id" INTEGER PRIMARY KEY  NOT NULL ,'
+        '"id" INTEGER PRIMARY KEY NOT NULL,'
         '"name" TEXT NOT NULL,'
         '"item_name" TEXT NOT NULL,'
         '"class" TEXT NOT NULL,'
@@ -63,7 +63,7 @@ def main():
     dbc.execute('CREATE TABLE "new_tf2idb_equip_regions" ("id" INTEGER NOT NULL , "region" TEXT NOT NULL , PRIMARY KEY ("id", "region"))')
     dbc.execute('CREATE TABLE "new_tf2idb_capabilities"  ("id" INTEGER NOT NULL , "capability" TEXT NOT NULL )')
     dbc.execute('CREATE TABLE "new_tf2idb_attributes" ('
-        '"id" INTEGER PRIMARY KEY  NOT NULL ,'
+        '"id" INTEGER PRIMARY KEY NOT NULL,'
         '"name" TEXT NOT NULL,'
         '"attribute_class" TEXT,'
         '"attribute_type" TEXT,'
@@ -79,7 +79,7 @@ def main():
         '"apply_tag_to_item_definition" TEXT'
         ')'
     )
-    dbc.execute('CREATE TABLE "new_tf2idb_qualities" ("name" TEXT PRIMARY KEY  NOT NULL , "value" INTEGER NOT NULL )');
+    dbc.execute('CREATE TABLE "new_tf2idb_qualities" ("name" TEXT PRIMARY KEY  NOT NULL , "value" INTEGER NOT NULL )')
 
     nonce = int(time.time())
     dbc.execute('CREATE INDEX "tf2idb_item_attributes_%i" ON "new_tf2idb_item_attributes" ("attribute" ASC)' % nonce)
@@ -109,7 +109,8 @@ def main():
                 atype = 'float'
         attribute_type[v['name'].lower()] = (k, atype)
         dbc.execute('INSERT INTO new_tf2idb_attributes '
-            '(id,name,attribute_class,attribute_type,description_string,description_format,effect_type,hidden,stored_as_integer,armory_desc,is_set_bonus,is_user_generated,can_affect_recipe_component_name,apply_tag_to_item_definition) '
+            '(id,name,attribute_class,attribute_type,description_string,description_format,effect_type,hidden,stored_as_integer,armory_desc,is_set_bonus,'
+                'is_user_generated,can_affect_recipe_component_name,apply_tag_to_item_definition) '
             'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
             (k,v.get('name'),v.get('attribute_class'),v.get('attribute_type'),v.get('description_string'),v.get('description_format'),
                 v.get('effect_type'),v.get('hidden'),v.get('stored_as_integer'),v.get('armory_desc'),v.get('is_set_bonus'),
@@ -194,4 +195,4 @@ def main():
     db.commit()
     
 if __name__ == "__main__":
-    main();
+    main()
